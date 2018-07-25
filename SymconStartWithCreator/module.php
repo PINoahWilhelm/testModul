c357a74593bc9e84d56c26842a8b1b6c4152b84c
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-25 15:20:22 +0200

    no message

--- SymconStartWithCreator/module.php
+++ SymconStartWithCreator/module.php
@@ -2,7 +2,7 @@
 require(__DIR__ . "\\pimodule.php");
 
     // Klassendefinition
-    class SymconTestFromBase extends PISymconModule {
+    class SymconStartWithCreator extends PISymconModule {
 
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
